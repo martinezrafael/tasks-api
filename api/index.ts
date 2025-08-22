64dda667f0545b8749f24a53b8e3ecda40e964e2
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2025-08-21 22:01:01 -0300

    feat: implementa servidor NestJS com suporte a CORS e validação global

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "../app.module";
+import { ValidationPipe } from "@nestjs/common";
+
+let cachedServer: any;
+
+async function bootstrapServer() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.init();
+  return app.getHttpAdapter().getInstance();
+}
+
+export default async function handler(req: any, res: any) {
+  if (!cachedServer) cachedServer = await bootstrapServer();
+  return cachedServer(req, res);
+}
